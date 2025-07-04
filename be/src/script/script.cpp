5d308b9ac7d9e23394def67b7fe3edba090b5411
luohaha <18810541851@163.com>
2025-07-04 16:02:22 +0800

    [Feature] add manual vacuum and check
    Signed-off-by: luohaha <18810541851@163.com>

--- be/src/script/script.cpp
+++ be/src/script/script.cpp
@@ -34,6 +34,7 @@
 #include "storage/lake/tablet.h"
 #include "storage/lake/tablet_manager.h"
 #include "storage/lake/tablet_metadata.h"
+#include "storage/lake/vacuum.h"
 #include "storage/primary_key_dump.h"
 #include "storage/storage_engine.h"
 #include "storage/tablet.h"
@@ -307,6 +308,16 @@ public:
         RETURN_IF(!base64_decode(meta_base64, &meta_bytes), "bad base64 string");
         RETURN_IF(!pb.ParseFromString(meta_bytes), "parse encryption meta failed");
         return proto_to_json(pb);
+    }
+
+    static int64_t datafile_gc(std::string_view root_location, std::string_view audit_file_path,
+                               int64_t expired_seconds, bool do_delete) {
+        auto val_st = lake::datafile_gc(root_location, audit_file_path, expired_seconds, do_delete);
+        if (!val_st.ok()) {
+            LOG(WARNING) << "datafile_gc failed: " << val_st.status().to_string();
+            return -1;
+        }
+        return val_st.value();
     }
 
     static std::shared_ptr<TabletBasicInfo> get_tablet_info(int64_t tablet_id) {
@@ -598,6 +609,7 @@ public:
             REG_STATIC_METHOD(StorageEngineRef, ls_tablet_dir);
             REG_STATIC_METHOD(StorageEngineRef, set_error_state);
             REG_STATIC_METHOD(StorageEngineRef, recover_tablet);
+            REG_STATIC_METHOD(StorageEngineRef, datafile_gc);
         }
     }
 };
